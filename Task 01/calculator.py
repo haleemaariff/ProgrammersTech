1343fa7fbd6e5b67552b11d4a186ac6070c7dcf6
haleemaariff <haleemaarif009@gmail.com>
2024-07-05 01:28:57 +0500

    Update calculator.py

--- Task 01/calculator.py
+++ Task 01/calculator.py
@@ -78,7 +78,6 @@ if __name__ == "__main__":
 	Decimal= Button(gui, text='.', fg='black', bg='grey', 
 					command=lambda: press('.'), height=1, width=7) 
 	Decimal.grid(row=6, column=0) 
-	# start the GUI 
 	gui.mainloop() 
 
 
